0393a737290ba6e23582a238d0af2a07b2e68cde
HKP-791 <150802104+HKP-791@users.noreply.github.com>
2024-11-24 21:47:27 +0800

    auto_parking_simulation
    无人车和世界仿真

---
+++ auto_parking/README.md
@@ -0,0 +1,24 @@
+1.使用前要cd到auto_parking目录下source
+2.编写了两个.launch.py，分别用于启动rviz2和建立了世界的gazebo.可以通过下命令启动导入了无人机的rviz2和gazebo。
+```bash
+ros2 launch rosmaster_x3 display_gazebo.launch.py
+ros2 launch rosmaster_x3 display_rviz2.launch.py
+```
+3.无人车的urdf描述文件和仿真世界的world文件位于位于
+```text
+/auto_parking/src/rosmaster_x3/urdf/rosmaster_x3.urdf
+/auto_parking/src/rosmaster_x3/world/auto_parking.world
+```
+4.目前无人车已经实现了普通相机、深度相机、激光雷达、imu惯性测量单元、tf里程记和键盘控制行进。不过现在的键盘控制还是以前轮转向后轮固定的方式运行。普通相机、深度相机、激光雷达、imu惯性测量单元、tf里程记的话题分别为
+```text
+/camera_sensor/image_raw
+/camera_sensor/depth/image_raw
+/scan
+/imu
+/odom
+```
+5.可以通过rviz2、rqt和gazegbo查看实时视频和雷达点云等数据（推荐rviz2）.操控小车移动可以通过调用节点进行控制
+```bash
+ros2 run teleop_twist_keyboard teleop_twist_keyboard
+```
+6.小车的转动惯量等物理特性还没做，等准备上代码训练的时候加。现在在做navigation2。
---
+++ auto_parking/src/rosmaster_x3/launch/display_gazebo.launch.py
@@ -0,0 +1,33 @@
+import os
+from launch import LaunchDescription
+from launch.actions import ExecuteProcess
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+
+def generate_launch_description():
+    robot_name_in_model = 'rosmaster_x3'
+    package_name = 'rosmaster_x3'
+    urdf_name = "rosmaster_x3.urdf"
+
+    ld = LaunchDescription()
+    pkg_share = FindPackageShare(package=package_name).find(package_name) 
+    urdf_model_path = os.path.join(pkg_share, f'urdf/{urdf_name}')
+    gazebo_world_path = os.path.join(pkg_share, 'world/auto_parking.world')
+
+    # Start Gazebo server
+    start_gazebo_cmd =  ExecuteProcess(
+        cmd=['gazebo', '--verbose','-s', 'libgazebo_ros_init.so', '-s', 'libgazebo_ros_factory.so', gazebo_world_path],
+        output='screen')
+
+    # Launch the robot
+    spawn_entity_cmd = Node(
+        package='gazebo_ros', 
+        executable='spawn_entity.py',
+        arguments=['-entity', robot_name_in_model,  '-file', urdf_model_path ], output='screen')
+
+    ld.add_action(start_gazebo_cmd)
+    ld.add_action(spawn_entity_cmd)
+
+
+    return ld
